652dc86a6a7ce9d339720c73557ba9a2d0e79187
shave <narcisoosilva@gmail.com>
2022-06-12 20:48:52 -0300

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
-	module.exports = function(){
+var connMySQL = function(){
+	console.log('conexao com bd foi estabelicida');
 		return mysql.createConnection({
 			host : 'localhost',
 			user : 'root',
 			password : '1234',
 			database : 'portal_noticias'
-		});
+	});
+}
+
+	module.exports = function(){
+		console.log('O autoload carregou o modulo de conexao com bd');
+		return connMySQL;
 	}
