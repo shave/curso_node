98089831c631e01643b989f7ea8aee9bc2e3d478
shave <narcisoosilva@gmail.com>
2022-06-13 19:52:45 -0300

    Update noticia.js
    Implementando o Models na aplicação

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -3,8 +3,9 @@ module.exports = function(app){
 	app.get('/noticia', function(req, res){
 		
 		var connection = app.config.dbConnection();
+		var noticiasModel = app.app.models.noticiasModel;
 
-		connection.query('select * from noticias where id_noticia = 1;', function(error, result){
+		noticiasModel.getNoticias(connection, function(error, result){
 			res.render("noticias/noticia", {noticia : result});	
 		});	
 	});
