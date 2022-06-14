14eddfe17513ed2891692b8809ccf63cac77e43c
shave <narcisoosilva@gmail.com>
2022-06-14 20:34:19 -0300

    Update noticias.js
    Criando classes em js

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -3,7 +3,7 @@ module.exports = function(app){
 	app.get('/noticias', function(req, res){
 		
 		var connection = app.config.dbConnection();
-		var noticiasModel = app.app.models.noticiasModel;
+		var noticiasModel = new app.app.models.NoticiaDao;
 
 		noticiasModel.getNoticias(connection, function(error, result){
 			res.render("noticias/noticias", {noticias : result});
