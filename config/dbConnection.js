45c67a5aff5a00ef83334d69ef2cd60bd51758e3
shave <narcisoosilva@gmail.com>
2022-06-14 20:34:39 -0300

    Update dbConnection.js
    Criando classes em js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,6 @@
 var mysql = require('mysql');
 
 var connMySQL = function(){
-	console.log('conexao com bd foi estabelicida');
 		return mysql.createConnection({
 			host : 'localhost',
 			user : 'root',
@@ -11,6 +10,5 @@ var connMySQL = function(){
 }
 
 	module.exports = function(){
-		console.log('O autoload carregou o modulo de conexao com bd');
 		return connMySQL;
 	}
