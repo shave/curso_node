bae5ea09412bda1bb6ee0918c02813f918a4fe4d
shave <narcisoosilva@gmail.com>
2022-06-14 21:08:25 -0300

    Update admin.js
    implementando classes no js, correção de erro de sintaxe

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,13 +7,10 @@ module.exports = function(app) {
 		var noticia = req.body;
 
 		var connection = app.config.dbConnection();
-		var noticiasModel = new app.app.models.NoticiaDAO;
+		var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
-		noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+		noticiasModel.salvarNoticia(noticia, function(error, result){
 			res.redirect('/noticias');
 		});	
-
-
-
 	});
 };
