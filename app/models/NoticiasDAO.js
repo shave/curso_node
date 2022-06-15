de7b8b88e44e296379bcd27119cb9587190cc320
shave <narcisoosilva@gmail.com>
2022-06-14 21:07:57 -0300

    Update NoticiasDAO.js
    implementando classes no js

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -1,18 +1,18 @@
-function NoticiasDAO(){
-
+function NoticiasDAO(connection){
+	this._connection = connection;
 }
 
-NoticiasDAO.prototype.getNoticias = function(connection, callback){
-		connection.query('select * from noticias', callback); 
-	}
+NoticiasDAO.prototype.getNoticias = function(callback){
+	this._connection.query('select * from noticias', callback); 
+}
 
-NoticiasDAO.prototype.getNoticia = function(connection, callback){
-		connection.query('select * from noticias where id_noticia = 2',callback);
-	}
+NoticiasDAO.prototype.getNoticia = function(callback){
+	this._connection.query('select * from noticias where id_noticia = 3',callback);
+}
 
-NoticiasDAO.prototype.salvarNoticia = function(noticia, connection, callback) {
-		connection.query('insert into noticias set ?',noticia, callback)	
-	}
+NoticiasDAO.prototype.salvarNoticia = function(noticia, callback) {
+	this._connection.query('insert into noticias set ?',noticia, callback)	
+}
 
 module.exports = function(){
 	return NoticiasDAO;
