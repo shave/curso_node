93f63c47478d7c249920d8fcb9b10571447df43a
shave <narcisoosilva@gmail.com>
2022-06-14 21:08:55 -0300

    Update noticia.js
    implementando classes no js, correção de erro de sintaxe

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -3,9 +3,9 @@ module.exports = function(app){
 	app.get('/noticia', function(req, res){
 		
 		var connection = app.config.dbConnection();
-		var noticiasModel = new app.app.models.NoticiaDAO;
+		var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
-		noticiasModel.getNoticias(connection, function(error, result){
+		noticiasModel.getNoticia(function(error, result){
 			res.render("noticias/noticia", {noticia : result});	
 		});	
 	});
